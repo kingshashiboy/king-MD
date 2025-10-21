251ac4bbc9d99ba4138d49e7b5dc94805c12fa02
kingshashiboy <agni119.67@gmail.com>
2025-10-21 19:00:52 +0530

    ☘️Shashika ☘️♻️𝐀𝐆𝐍𝐈♻️

--- config.js
+++ config.js
@@ -9,5 +9,7 @@ function convertToBool(text, fault = "true") {
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "suho~TI13RCgA#I9T67tPJMZtbkmWI0N_5OWpgb4PginmMNWiQFETnCL4",
   ALIVE_MSG: process.env.ALIVE_MSG || "☘️I made bot successful,bot is online ☘️",
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/ue4ppc.jpg"
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/ue4ppc.jpg",
+  BOT_NAME: process.env.BOT_NAME || "𝐀𝐆𝐍𝐈",
+  MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/8bkx4q.jpg"
 };
