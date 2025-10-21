fe82d57c9d569861417d349d45280d7a59dc3c7d
kingshashiboy <agni119.67@gmail.com>
2025-10-21 20:27:30 +0530

    ☘️Shashika ☘️♻️𝐀𝐆𝐍𝐈♻️

--- config.js
+++ config.js
@@ -11,5 +11,7 @@ module.exports = {
   ALIVE_MSG: process.env.ALIVE_MSG || "☘️I made bot successful,bot is online ☘️",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/ue4ppc.jpg",
   BOT_NAME: process.env.BOT_NAME || "𝐀𝐆𝐍𝐈",
-  MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/8bkx4q.jpg"
+  OWNER_NAME: process.env.OWNER_NAME || "shashika dilshan",
+  MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/8bkx4q.jpg",
+  FOOTER: process.env.FOOTER || "𝐀𝐆𝐍𝐈"
 };
