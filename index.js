eaff0433e42289d29077d4c30cb607b50302ebf7
kingshashiboy <agni119.67@gmail.com>
2025-10-24 17:34:01 +0530

    ☘️Shashika ☘️♻️𝐀𝐆𝐍𝐈♻️

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('☘️Bot connected to whatsapp ✅')
 
 let up = `☘️Wa-BOT connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://files.catbox.moe/4kux2y.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://mega.nz/file/pJUyASQR#yNoLh2jXd1Ml4sdJzpDpBLHYVWmx4jPay6SkOzHDx1k` }, caption: up })
 
 }
 })
