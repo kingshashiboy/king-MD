80cff07dd4e9fcf7a267e0aafdc09e4625212bd8
kingshashiboy <agni119.67@gmail.com>
2025-10-24 18:10:11 +0530

    ☘️Shashika ☘️♻️𝐀𝐆𝐍𝐈♻️

--- config.js
+++ config.js
@@ -12,6 +12,7 @@ module.exports = {
   ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/ue4ppc.jpg",
   BOT_NAME: process.env.BOT_NAME || "𝐀𝐆𝐍𝐈",
   OWNER_NAME: process.env.OWNER_NAME || "shashika dilshan",
-  MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/8bkx4q.jpg",
-  FOOTER: process.env.FOOTER || "𝐀𝐆𝐍𝐈"
+  MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://raw.githubusercontent.com/kingshashiboy/king-MD/refs/heads/main/all/alive/Picsart_25-09-06_12-15-06-946.jpg",
+  FOOTER: process.env.FOOTER || "𝐀𝐆𝐍𝐈",
+  DESCRIPTION: process.env.DESCRIPTION || "𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 𝐬𝐡𝐚𝐬𝐡𝐢𝐤𝐚 𝐝𝐢𝐥𝐬𝐡𝐚𝐧"
 };
