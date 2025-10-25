4e40a7dd0c6766f033375c3eba92a539a80bc511
kingshashiboy <agni119.67@gmail.com>
2025-10-25 20:11:31 +0530

    ☘️Shashika ☘️♻️𝐀𝐆𝐍𝐈♻️

--- plugins/autosticker.js
+++ plugins/autosticker.js
@@ -13,7 +13,7 @@ async (conn, mek, m, { from, body }) => {
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
             if (config.AUTO_STICKER === 'true') {
-                const stickerPath = path.join(__dirname, '../shashikamedia/autosticker', data[text]);
+                const stickerPath = path.join(__dirname, '../shashikameda/autosticker', data[text]);
 
                 if (fs.existsSync(stickerPath)) {
                     const stickerBuffer = fs.readFileSync(stickerPath);
