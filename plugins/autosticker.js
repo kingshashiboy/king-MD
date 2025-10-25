15b55dc2494697040dbbf2209e4efd15e7a88bd5
kingshashiboy <agni119.67@gmail.com>
2025-10-25 18:24:13 +0530

    ☘️Shashika ☘️♻️𝐀𝐆𝐍𝐈♻️

---
+++ plugins/autosticker.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+const config = require('../config');
+const { cmd } = require('../command');
+
+cmd({
+  on: "body"
+},
+async (conn, mek, m, { from, body }) => {
+    const filePath = path.join(__dirname, '../all/autosticker.json');
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+    for (const text in data) {
+        if (body.toLowerCase() === text.toLowerCase()) {
+            if (config.AUTO_STICKER === 'true') {
+                const stickerPath = path.join(__dirname, '../shashikamedia/autosticker', data[text]);
+
+                if (fs.existsSync(stickerPath)) {
+                    const stickerBuffer = fs.readFileSync(stickerPath);
+
+                    await conn.sendMessage(from, {
+                        sticker: stickerBuffer,
+                        packname: '𝐬𝐡𝐚𝐬𝐡𝐢𝐤𝐚 𝐝𝐢𝐥𝐬𝐡𝐚𝐧',
+                        author: 'Shashika dil'
+                    }, { quoted: mek });
+                } else {
+                    console.warn(`Sticker not found: ${stickerPath}`);
+                }
+            }
+        }
+    }
+});
+                      
